09e2de1ef2dc7986deb8bfdeba46d2a6a3a8a226
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-05 10:10:02 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -1,27 +1,30 @@
 <?php
 require 'config/function.php';
 
-if (isset($_POST['loginBtn'])) {
+if (isset($_POST['loginBtn']))
+{
+
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
 
-    if ($email != '' && $password != '') {
+    if($email != '' && $password != '')
+     {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
-        
-        if ($result) {
-            if (mysqli_num_rows($result) == 1) {
+        if ($result){
+
+            if (mysqli_num_rows($result) == 1){
+                
                 $row = mysqli_fetch_assoc($result);
                 $hasedPassword = $row['password'];
 
-                if (!password_verify($password, $hasedPassword)) {
-                    header('Location: login.php?alert=error&message=Invalid Password');
-                    exit();
+                
+                if(!password_verify($password,$hasedPassword)){
+                    redirect('login.php','Invalid Password');
                 }
 
-                if ($row['is_ban'] == 1) {
-                    header('Location: login.php?alert=error&message=Your account has been banned. Contact your Admin.');
-                    exit();
+                if($row['is_ban'] == 1){
+                    redirect('login.php','Your account has been banned. Contact your Admin.');
                 }
 
                 $_SESSION['loggedIn'] = true;
@@ -30,21 +33,23 @@ if (isset($_POST['loginBtn'])) {
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-                ];
 
-                header('Location: login.php?alert=success');
-                exit();
-            } else {
-                header('Location: login.php?alert=error&message=Invalid Email Address');
-                exit();
+                ];
+                    
+                redirect('admin/index.php','Logged In Successfully');
+                    
+            }else{
+                    redirect('login.php', 'Invalid Email Address');
             }
-        } else {
-            header('Location: login.php?alert=error&message=Something Went Wrong!');
-            exit();
+
+        }else{
+                redirect('login.php', 'Something Went Wrong!');
         }
-    } else {
-        header('Location: login.php?alert=error&message=All fields are mandatory!');
-        exit();
     }
+    else 
+    {
+          redirect('login.php', 'All fields are mandetory!');
+    }
+    
 }
 ?>
