65824b0da2b7057876b79440372a2c91dc397bc3
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-05 10:04:16 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -1,55 +1,11 @@
 <?php
-require 'config/function.php';
+// Your login logic here...
 
-if (isset($_POST['loginBtn']))
-{
-
-    $email = validate($_POST['email']);
-    $password = validate($_POST['password']);
-
-    if($email != '' && $password != '')
-     {
-        $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
-        $result = mysqli_query($conn, $query);
-        if ($result){
-
-            if (mysqli_num_rows($result) == 1){
-                
-                $row = mysqli_fetch_assoc($result);
-                $hasedPassword = $row['password'];
-
-                
-                if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
-                }
-
-                if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
-                }
-
-                $_SESSION['loggedIn'] = true;
-                $_SESSION['loggedInUser'] = [
-                    'user_id' => $row['id'],
-                    'name' => $row['name'],
-                    'email' => $row['email'],
-                    'phone' => $row['phone'],
-
-                ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
-            }
-
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
-        }
-    }
-    else 
-    {
-          redirect('login.php', 'All fields are mandetory!');
-    }
-    
+if ($login_successful) {
+    header('Location: login.php?alert=success');
+    exit();
+} else {
+    header('Location: login.php?alert=error');
+    exit();
 }
 ?>
