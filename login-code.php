c866edfe0f97198410e594cd549ff889a9099adf
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-05 09:51:53 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -1,30 +1,31 @@
 <?php
 require 'config/function.php';
 
-if (isset($_POST['loginBtn']))
-{
+if (isset($_POST['loginBtn'])) {
 
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
 
-    if($email != '' && $password != '')
-     {
+    if($email != '' && $password != '') {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
-        if ($result){
+        if ($result) {
 
-            if (mysqli_num_rows($result) == 1){
+            if (mysqli_num_rows($result) == 1) {
                 
                 $row = mysqli_fetch_assoc($result);
                 $hasedPassword = $row['password'];
 
-                
-                if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
+                if(!password_verify($password, $hasedPassword)) {
+                    $_SESSION['alert'] = 'Invalid Password';
+                    header('Location: login.php');
+                    exit();
                 }
 
-                if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
+                if($row['is_ban'] == 1) {
+                    $_SESSION['alert'] = 'Your account has been banned. Contact your Admin.';
+                    header('Location: login.php');
+                    exit();
                 }
 
                 $_SESSION['loggedIn'] = true;
@@ -33,23 +34,27 @@ if (isset($_POST['loginBtn']))
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-
                 ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
+
+                $_SESSION['alert'] = 'Logged In Successfully';
+                header('Location: login.php');
+                exit();
+                
+            } else {
+                $_SESSION['alert'] = 'Invalid Email Address';
+                header('Location: login.php');
+                exit();
             }
 
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
+        } else {
+            $_SESSION['alert'] = 'Something Went Wrong!';
+            header('Location: login.php');
+            exit();
         }
+    } else {
+        $_SESSION['alert'] = 'All fields are mandatory!';
+        header('Location: login.php');
+        exit();
     }
-    else 
-    {
-          redirect('login.php', 'All fields are mandetory!');
-    }
-    
 }
 ?>
