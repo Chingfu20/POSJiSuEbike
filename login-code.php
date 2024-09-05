30394892017576c63fe4565f4c64603073ac1060
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-05 09:38:50 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -36,7 +36,7 @@ if (isset($_POST['loginBtn']))
 
                 ];
                     
-                redirect('admin/index.php','Logged In Successfully');
+                redirect('admin/login.php','Logged In Successfully');
                     
             }else{
                     redirect('login.php', 'Invalid Email Address');
