1917a3e0ed63ab340b6ab89100685a3bb4870bd7
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-05 09:23:08 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,12 @@
-<?php 
-
+<?php
 require 'config/function.php';
 
-if(isset($_SESSION['loggedIn'])){
-
+if (isset($_SESSION['loggedIn'])) {
     logoutSession();
-    redirect('login.php','Logged Out Successfully.');
+    redirect('login.php', 'Logged Out Successfully.');
+} elseif (isset($_GET['login_success'])) {
+    redirect('admin/index.php', 'Logged In Successfully');
+} else {
+    redirect('login.php', 'Please log in.');
 }
-
 ?>
