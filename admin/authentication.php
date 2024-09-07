0a26c8ad7e36450e1896f1aa39046ff03b4255f7
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-07 13:24:36 +0800

    Update authentication.php

--- admin/authentication.php
+++ admin/authentication.php
@@ -1,9 +1,11 @@
 <?php
 
-session_start(); 
+session_start(); // Ensure session is started
+
+// Assuming $conn is your database connection
 
 if (isset($_SESSION['loggedInUser'])) {
-    $email = validate($_SESSION['loggedInUser']['email']); 
+    $email = validate($_SESSION['loggedInUser']['email']); // Fix the typo 'loggedInUSer' to 'loggedInUser'
 
     $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
     $result = mysqli_query($conn, $query);
@@ -17,6 +19,7 @@ if (isset($_SESSION['loggedInUser'])) {
             logoutSession();
             redirect('../login.php', 'Your account has been banned! Please contact admin.');
         }
+        // Additional login validation can go here if needed
     }
 } else {
     redirect('../login.php', 'Login to Continue...');
