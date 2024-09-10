1a55d866e1f0709ec52c0e98d747039c122ecc93
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-10 18:54:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -122,13 +122,7 @@
     </div>
 </div>
 
-
 <!-- Contact Us Section -->
- <!-- Motorcycle animation with adjusted size -->
-<div class="text-center mt-4" data-aos="zoom-in">
-    <img src="assets/images/ride_01.gif" alt="Person driving a motorcycle" class="img-fluid" style="max-width: 300px; height: auto;">
-</div>
-
 <div class="container py-5 section-background">
     <h2 class="text-center text-white" data-aos="fade-up">Contact Us</h2>
     <div class="row text-center text-white">
@@ -145,6 +139,10 @@
             <p>0947-209-8888</p>
         </div>
     </div>
+   <!-- Motorcycle animation with adjusted size -->
+<div class="text-center mt-4" data-aos="zoom-in">
+    <img src="assets/images/ride_01.gif" alt="Person driving a motorcycle" class="img-fluid" style="max-width: 300px; height: auto;">
+</div>
 
 
 <?php include('includes/footer.php'); ?>
