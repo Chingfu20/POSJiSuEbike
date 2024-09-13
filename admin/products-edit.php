3f3727f3218d9c2bdf0f454dd91d5043fdea1f35
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-13 11:52:15 +0800

    Update products-edit.php

--- admin/products-edit.php
+++ admin/products-edit.php
@@ -79,10 +79,9 @@
                          <input type="file" name="image" class="form-control" />
                          <img src="../<?= $product['data']['image']; ?>" style="width:40px;height:40px;" alt="Img" />
                     </div>
-                    <div class="col-md-6 mb-3 d-flex justify-content-end">
-                        <br/>
+                    <div class="d-flex justify-content-end mt-3">
                          <button type="submit" name="updateProduct" class="btn btn-primary">Update</button>
-                      </div>
+                    </div>
                    </div>
                    <?php                        
                    }
