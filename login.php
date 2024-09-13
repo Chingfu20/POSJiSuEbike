61e24e71cde174d4ae95e001e1fe1f93942fbf72
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-13 11:30:31 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JiSu Ebike POS System</title>
-    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <?php 
