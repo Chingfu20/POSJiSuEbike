3e00962bed1f8d29ba0df1c1b605e4c818923f25
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-13 10:21:34 +0800

    commit

--- login.php
+++ login.php
@@ -17,36 +17,26 @@
     }
     ?>
 
-    <div class="py-5 bg-light">
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card shadow rounded-4 custom-card">
-
-                        <?php alertMessage(); ?>
-
-                        <div class="p-5">
-                            <center><h4 class="text-dark mb-3">Login Admin</h4></center>
-                            <form action="login-code.php" method="POST" class="login-form">
-                                
-                                <div class="mb-3">
-                                    <label class="form-label">Enter Email</label>
-                                    <input type="email" name="email" class="form-control" required />
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Enter Password</label>
-                                    <input type="password" name="password" class="form-control" required />
-                                </div>
-                                <div class="my-3">
-                                    <button type="submit" name="loginBtn" class="btn btn-primary w-100 mt-2">
-                                        Sign In
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+    <div class="login-container">
+        <div class="login-card">
+            <center><h4 class="login-title">Login</h4></center>
+            <form action="login-code.php" method="POST" class="login-form">
+                <div class="login-input-group">
+                    <input type="email" name="email" class="login-input" placeholder="Username" required />
                 </div>
-            </div>
+                <div class="login-input-group">
+                    <input type="password" name="password" class="login-input" placeholder="Password" required />
+                </div>
+                <div class="my-3">
+                    <button type="submit" name="loginBtn" class="login-button w-100 mt-2">
+                        Login
+                    </button>
+                </div>
+                <div class="login-options">
+                    <label><input type="checkbox" name="remember" /> Remember Me</label>
+                    <a href="#" class="forgot-password">Forgot password?</a>
+                </div>
+            </form>
         </div>
     </div>
 </body>
