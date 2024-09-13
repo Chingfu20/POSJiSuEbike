6566fec7f352915b8c8e3f3085506cabad45c895
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-13 11:30:36 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -3,28 +3,28 @@ require 'config/function.php';
 
 if (isset($_POST['loginBtn']))
 {
-
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
 
-    if($email != '' && $password != '')
-     {
+    if ($email != '' && $password != '')
+    {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
-        if ($result){
-
-            if (mysqli_num_rows($result) == 1){
-                
+        if ($result)
+        {
+            if (mysqli_num_rows($result) == 1)
+            {
                 $row = mysqli_fetch_assoc($result);
-                $hasedPassword = $row['password'];
+                $hashedPassword = $row['password'];
 
-                
-                if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
+                if (!password_verify($password, $hashedPassword))
+                {
+                    redirect('login.php', 'Invalid Password');
                 }
 
-                if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
+                if ($row['is_ban'] == 1)
+                {
+                    redirect('login.php', 'Your account has been banned. Contact your Admin.');
                 }
 
                 $_SESSION['loggedIn'] = true;
@@ -33,23 +33,35 @@ if (isset($_POST['loginBtn']))
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-
                 ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
-            }
 
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
+                // Output SweetAlert script
+                echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
+                echo "<script>
+                        Swal.fire({
+                            title: 'Logged In Successfully',
+                            icon: 'success',
+                            confirmButtonText: 'OK'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = 'admin/index.php';
+                            }
+                        });
+                      </script>";
+            }
+            else
+            {
+                redirect('login.php', 'Invalid Email Address');
+            }
+        }
+        else
+        {
+            redirect('login.php', 'Something Went Wrong!');
         }
     }
-    else 
+    else
     {
-          redirect('login.php', 'All fields are mandetory!');
+        redirect('login.php', 'All fields are mandatory!');
     }
-    
 }
 ?>
