f12862a921bcc7e1612eec5dea8a70f80a789da2
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-13 15:13:51 +0800

    Update order-create.php

--- admin/order-create.php
+++ admin/order-create.php
@@ -139,7 +139,7 @@ foreach ($sessionProducts as $key => $item) :
                     </div>
                         <div class="col-md-4">
                         <label for="cphone">Enter Customer Phone Number</label>
-                        <input type="text" id="cphone" class="form-control" maxlength="13" pattern="\d{11}" title="Enter exactly 13 digits" />
+                        <input type="text" id="cphone" class="form-control" maxlength="13" pattern="\d{13}" title="Enter exactly 13 digits" />
                     </div>
                         <div class="col-md-4">
                         <label>Total Amount</label>
