d396c7c7e09cb5da0c3669f73720dfed25f4b060
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-18 09:43:37 +0800

    Update customers-create.php

--- admin/customers-create.php
+++ admin/customers-create.php
@@ -1,40 +1,44 @@
 <?php include('includes/header.php'); ?>
 
 <div class="container-fluid px-4">
-        <div class="card mt-4 shadow-sm">
-          <div class="card-header">
-               <h4 class="mb-0">Add Customer
-                    <a href="customers.php" class="btn btn-danger float-end">Back</a>
-               </h4>
-          </div>
-          <div class="card-body">
+    <div class="card mt-4 shadow-sm">
+        <div class="card-header">
+            <h4 class="mb-0">Add Customer
+                <a href="customers.php" class="btn btn-danger float-end">Back</a>
+            </h4>
+        </div>
+        <div class="card-body">
             
-               <?php alertMessage(); ?>
+            <?php alertMessage(); ?>
 
-               <form action="code.php" method="POST">
-                    
-                   <div class="row">
+            <form action="code.php" method="POST">
+                
+                <div class="row">
                     <div class="col-md-12 mb-3">
-                         <label for="">Name *</label>
-                         <input type="text" name="name" required class="form-control" />
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
                     </div>
                     <div class="col-md-12 mb-3">
-                         <label for="">Email Id</label>
-                         <input type="email" name="email" class="form-control" />
+                        <label for="">Email Id</label>
+                        <input type="email" name="email" class="form-control" />
                     </div>
                     <div class="col-md-12 mb-3">
-                         <label for="">Phone </label>
-                         <input type="number" name="phone" class="form-control" />
+                        <label for="">Phone</label>
+                        <input type="number" name="phone" class="form-control" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Address</label>
+                        <textarea name="address" class="form-control" rows="3" required></textarea>
                     </div>
                     <div class="d-flex justify-content-end mt-3">
                         <br/>
-                         <button type="submit" name="saveCustomer" class="btn btn-primary">Save</button>
+                        <button type="submit" name="saveCustomer" class="btn btn-primary">Save</button>
                     </div>
                 </div>
 
-               </form>
-          </div>
+            </form>
         </div>
-     </div>
+    </div>
+</div>
 
 <?php include('includes/footer.php'); ?>
