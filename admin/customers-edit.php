49e3cad77d016804246a10f5e31fa9d523e29226
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-18 10:18:04 +0800

    Update customers-edit.php

--- admin/customers-edit.php
+++ admin/customers-edit.php
@@ -27,29 +27,28 @@
 
                        <input type="hidden" name="customerId" value="<?= $customer['data']['id']; ?>" />
 
-                         <div class="row">
-                    <div class="col-md-12 mb-3">
-                         <label for="">Name *</label>
-                         <input type="text" name="name" required value="<?= $customer['data']['name']; ?>" required class="form-control" />
-                    </div>
-                    <div class="col-md-12 mb-3">
-                         <label for="">Email Id</label>
-                         <input type="email" name="email"  value="<?= $customer['data']['email']; ?>" class="form-control" />
-                    </div>
-                    <div class="col-md-12 mb-3">
-                         <label for="">Phone </label>
-                         <input type="number" name="phone" value="<?= $customer['data']['phone']; ?>" class="form-control" />
-                    </div>
-                    <div class="col-md-6">
-                         <label>Status (UnChecked=Visible, Checked=Hidden)</label>
-                         <br/>
-                         <input type="checkbox" name="status" <?= $customer['data']['status'] == true ? 'checked':''; ?> style="width:30px;height:30px;";>
-                    </div>
-                    <div class="col-md-6 mb-3 text-end">
-                        <br/>
-                         <button type="submit" name="updateCustomer" class="btn btn-primary">update</button>
-                    </div>
-                </div>
+                       <div class="row">
+    <div class="col-md-12 mb-3">
+        <label for="">Name *</label>
+        <input type="text" name="name" required value="<?= $customer['data']['name']; ?>" class="form-control" />
+    </div>
+    <div class="col-md-12 mb-3">
+        <label for="">Email Id</label>
+        <input type="email" name="email" value="<?= $customer['data']['email']; ?>" class="form-control" />
+    </div>
+    <div class="col-md-12 mb-3">
+        <label for="">Phone</label>
+        <input type="number" name="phone" value="<?= $customer['data']['phone']; ?>" class="form-control" />
+    </div>
+    <div class="col-md-12 mb-3">
+        <label for="">Address *</label>
+        <textarea name="address" class="form-control" rows="3" required><?= $customer['data']['address']; ?></textarea>
+    </div>
+    <div class="d-flex justify-content-end mt-3">
+        <br />
+        <button type="submit" name="updateCustomer" class="btn btn-primary">Update</button>
+    </div>
+</div>
                 <?php
                    }
                    else
