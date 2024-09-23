29d21d937fdf27bda9fa10eafd74007cb2d865a5
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-23 10:47:31 +0800

    commit

--- login.php
+++ login.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JiSu Ebike POS System</title>
-    <link rel="stylesheet" href="login.css">
+    <title>JiSu Ebike POS System - Login</title>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <?php 
-    include('includes/header.php'); 
 
-    if(isset($_SESSION['loggedIn'])){
+<body>
+    <?php
+    include('includes/header.php');
+
+    // If the user is already logged in, redirect to index.php
+    if (isset($_SESSION['loggedIn'])) {
         ?>
         <script>window.location.href = 'index.php';</script>
         <?php
+        exit();
     }
     ?>
 
-    <div class="py-5 bg-light">
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <div class="card shadow rounded-4 custom-card">
-
-                        <div class="p-5">
-                            <center><h4 class="text-dark mb-3">Login Admin</h4></center>
-                            <form action="login-code.php" method="POST" class="login-form">
-                                
-                                <div class="mb-3">
-                                    <label class="form-label">Enter Email</label>
-                                    <input type="email" name="email" class="form-control" required />
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Enter Password</label>
-                                    <input type="password" name="password" class="form-control" required />
-                                </div>
-                                <div class="my-3">
-                                    <button type="submit" name="loginBtn" class="btn btn-primary w-100 mt-2">
-                                        Sign In
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="login-code.php" method="POST">
+                    <h2>Login Admin</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" name="email" required>
+                        <label>Email</label>
                     </div>
-                </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="password" required>
+                        <label>Password</label>
+                    </div>
+                    <div class="forget">
+                        <label><input type="checkbox" name="remember"> Remember Me</label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button type="submit" name="loginBtn">Log In</button>
+                    <div class="register">
+                        <p>Don't have an account? <a href="#">Sign Up</a></p>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
+    </section>
 </body>
+
 </html>
