9d5e4842493271f2cfb786c3d31cef1b8a62ea82
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-23 11:24:42 +0800

    commit

--- login.php
+++ login.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JiSu Ebike POS System</title>
     <link rel="stylesheet" href="login.css">
+    <style>
+        body {
+            background: url('assets/images/ebike.jpg') no-repeat center center fixed;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
     <?php 
