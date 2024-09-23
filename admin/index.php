cf10a737092316703a9c338b6065096141f04fee
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-23 23:02:26 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -250,7 +250,7 @@ document.addEventListener("DOMContentLoaded", function () {
         data: {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             datasets: [{
-                label: 'Total Sales (in a month)',
+                label: 'Total Sales (in every month)',
                 data: monthlySales,  // Use dynamic sales data
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
