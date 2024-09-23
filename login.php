5968ea135d6bf4edd9d08c3420e83cb5d37b4d1a
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-23 11:42:21 +0800

    commit

--- login.php
+++ login.php
@@ -17,28 +17,32 @@
     }
     ?>
 
-    <div class="py-5 bg-light">
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-4 col-lg-3">
-                    <div class="card shadow rounded-4 custom-card">
-                        <div class="p-4">
-                            <h4 class="text-dark text-center mb-4">Admin Login</h4>
-                            <form action="login-code.php" method="POST" class="login-form">
-                                <div class="mb-3">
-                                    <label class="form-label">Email Address</label>
-                                    <input type="email" name="email" class="form-control" required />
+    <div class="bg-image">
+        <div class="overlay">
+            <div class="py-5">
+                <div class="container mt-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-4 col-lg-3">
+                            <div class="card shadow rounded-4 custom-card">
+                                <div class="p-4">
+                                    <h4 class="text-dark text-center mb-4">Admin Login</h4>
+                                    <form action="login-code.php" method="POST" class="login-form">
+                                        <div class="mb-3">
+                                            <label class="form-label">Email Address</label>
+                                            <input type="email" name="email" class="form-control" required />
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control" required />
+                                        </div>
+                                        <div class="my-3">
+                                            <button type="submit" name="loginBtn" class="btn btn-primary w-100">
+                                                Sign In
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Password</label>
-                                    <input type="password" name="password" class="form-control" required />
-                                </div>
-                                <div class="my-3">
-                                    <button type="submit" name="loginBtn" class="btn btn-primary w-100">
-                                        Sign In
-                                    </button>
-                                </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
