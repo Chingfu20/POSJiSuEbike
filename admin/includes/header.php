742bced5d8ffb7283b205cab636010eec44b1827
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-23 21:57:49 +0800

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -5,8 +5,7 @@ require 'authentication.php';
 <!DOCTYPE html>
 <html lang="en">
     <head>
-<link rel="icon" type="image/x-icon" href="assets/img/logo.jpg">
-
+        <link rel="icon" type="image/x-icon" href="assets/img/logo.jpg">
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -14,20 +13,18 @@ require 'authentication.php';
         <meta name="author" content="" />
         <title>JiSu E-Bike POS </title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-
         <link href="assets/css/styles.css" rel="stylesheet" />
-        
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
-      
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/default.min.css"/>
-
         <link href="assets/css/custom.css" rel="stylesheet" />
-
         
+        <style>
+            body {
+                background-color: #4D7BC3; /* Set the background color here */
+            }
+        </style>
     </head>
 
     <body class="sb-nav-fixed">
@@ -35,7 +32,6 @@ require 'authentication.php';
     <?php include('navbar.php'); ?>
 
     <div id="layoutSidenav">
-
        <?php include('sidebar.php'); ?>
 
           <div id="layoutSidenav_content">
