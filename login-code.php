48100565087edea9cbbb0d38549f6e6d583f3378
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-25 20:03:54 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -3,28 +3,48 @@ require 'config/function.php';
 
 if (isset($_POST['loginBtn']))
 {
-
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
 
     if($email != '' && $password != '')
-     {
+    {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
         if ($result){
 
             if (mysqli_num_rows($result) == 1){
-                
+
                 $row = mysqli_fetch_assoc($result);
                 $hasedPassword = $row['password'];
 
-                
                 if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
+                    echo "
+                    <script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Invalid Password!',
+                            allowOutsideClick: false
+                        }).then(function() {
+                            window.location.href = 'login.php';
+                        });
+                    </script>";
+                    exit;
                 }
 
                 if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
+                    echo "
+                    <script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Account Banned',
+                            text: 'Your account has been banned. Contact your Admin.',
+                            allowOutsideClick: false
+                        }).then(function() {
+                            window.location.href = 'login.php';
+                        });
+                    </script>";
+                    exit;
                 }
 
                 $_SESSION['loggedIn'] = true;
@@ -33,23 +53,65 @@ if (isset($_POST['loginBtn']))
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-
                 ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
+
+                echo "
+                <script>
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success!',
+                        text: 'Logged In Successfully!',
+                        allowOutsideClick: false
+                    }).then(function() {
+                        window.location.href = 'admin/index.php';
+                    });
+                </script>";
+                exit;
+
+            } else {
+                echo "
+                <script>
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Invalid Email Address!',
+                        allowOutsideClick: false
+                    }).then(function() {
+                        window.location.href = 'login.php';
+                    });
+                </script>";
+                exit;
             }
 
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
+        } else {
+            echo "
+            <script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something Went Wrong!',
+                    allowOutsideClick: false
+                }).then(function() {
+                    window.location.href = 'login.php';
+                });
+            </script>";
+            exit;
         }
     }
     else 
     {
-          redirect('login.php', 'All fields are mandetory!');
+        echo "
+        <script>
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning',
+                text: 'All fields are mandatory!',
+                allowOutsideClick: false
+            }).then(function() {
+                window.location.href = 'login.php';
+            });
+        </script>";
+        exit;
     }
-    
 }
 ?>
