a02ae4de445eb022ed4136db73461bfc44f07d51
Chingfu20 <jarinaangelo36@gmail.com>
2024-09-25 11:27:20 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -3,28 +3,47 @@ require 'config/function.php';
 
 if (isset($_POST['loginBtn']))
 {
-
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
 
     if($email != '' && $password != '')
-     {
+    {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
-        if ($result){
 
-            if (mysqli_num_rows($result) == 1){
-                
+        if ($result)
+        {
+            if (mysqli_num_rows($result) == 1)
+            {
                 $row = mysqli_fetch_assoc($result);
-                $hasedPassword = $row['password'];
+                $hashedPassword = $row['password'];
 
-                
-                if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
+                if (!password_verify($password, $hashedPassword))
+                {
+                    echo "<script>
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: 'Invalid Password!'
+                            }).then((result) => {
+                                window.location.href = 'login.php';
+                            });
+                          </script>";
+                    exit();
                 }
 
-                if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
+                if ($row['is_ban'] == 1)
+                {
+                    echo "<script>
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: 'Your account has been banned. Contact your Admin.'
+                            }).then((result) => {
+                                window.location.href = 'login.php';
+                            });
+                          </script>";
+                    exit();
                 }
 
                 $_SESSION['loggedIn'] = true;
@@ -33,23 +52,59 @@ if (isset($_POST['loginBtn']))
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-
                 ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
-            }
 
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
+                echo "<script>
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success!',
+                            text: 'Logged In Successfully'
+                        }).then((result) => {
+                            window.location.href = 'admin/index.php';
+                        });
+                      </script>";
+                exit();
+            }
+            else
+            {
+                echo "<script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Invalid Email Address!'
+                        }).then((result) => {
+                            window.location.href = 'login.php';
+                        });
+                      </script>";
+                exit();
+            }
+        }
+        else
+        {
+            echo "<script>
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something Went Wrong!'
+                    }).then((result) => {
+                        window.location.href = 'login.php';
+                    });
+                  </script>";
+            exit();
         }
     }
-    else 
+    else
     {
-          redirect('login.php', 'All fields are mandetory!');
+        echo "<script>
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Warning',
+                    text: 'All fields are mandatory!'
+                }).then((result) => {
+                    window.location.href = 'login.php';
+                });
+              </script>";
+        exit();
     }
-    
 }
 ?>
