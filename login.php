4e3134c6cbc4b67df6318ac2fa603556c24dfec1
Chingfu20 <jarinaangelo36@gmail.com>
2024-10-11 20:22:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -105,17 +105,17 @@
                 text: decodeURIComponent(message.replace(/\+/g, ' ')) // Replaces + with space
             });
         }
-            const togglePassword = document.querySelector('#togglePassword');
-    const password = document.querySelector('#password');
+        const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
 
-    togglePassword.addEventListener('click', function (e) {
-        // Toggle the type attribute
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        
-        // Toggle the eye icon
-        this.classList.toggle('fa-eye-slash');
-    });
+        togglePassword.addEventListener("click", function () {
+            // Toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+
+            // Toggle the eye icon
+            this.classList.toggle("fa-eye-slash");
+        });
     </script>
 </body>
 </html>
