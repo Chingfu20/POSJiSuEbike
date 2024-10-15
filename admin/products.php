7fd5f1b67cf345b596f5da52285c2b1c684828df
Chingfu20 <jarinaangelo36@gmail.com>
2024-10-15 15:31:49 +0800

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -66,8 +66,6 @@
                                 <td><?= htmlspecialchars($item['name']) ?></td>
                                 <td>
                                 <div class="input-group qtyBox">
-                                    
-                                        <button class="input-group-text decrement">-</button>
                                         <input type="text" value="<?= $item['quantity']; ?>" class="qty quantityInput" />
                                         <button class="input-group-text increment">+</button>
                                     </div>
