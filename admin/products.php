bf2c1920306ed93cc3d1375b41503c532d42db47
Chingfu20 <jarinaangelo36@gmail.com>
2024-10-15 15:47:29 +0800

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -61,17 +61,16 @@
                             <tr>
                                 <td><?= $displayId++ ?></td> <!-- Display ID -->
                                 <td>
-                                    <div class="input-group qtyBox">
+                                    <img src="../<?= htmlspecialchars($item['image']); ?>" style="width:50px;height:50px;" alt="Img" />
+                                </td>
+                                <td><?= htmlspecialchars($item['name']) ?></td>
+                                <td>
+                                <div class="input-group qtyBox">
                                         <button class="input-group-text decrement">-</button>
                                         <input type="text" value="<?= $item['quantity']; ?>" class="qty quantityInput" />
                                         <button class="input-group-text increment">+</button>
                                     </div>
                                 </td>
-                                <td>
-                                    <img src="../<?= htmlspecialchars($item['image']); ?>" style="width:50px;height:50px;" alt="Img" />
-                                </td>
-                                <td><?= htmlspecialchars($item['name']) ?></td>
-                                <td class="text-center"><?= htmlspecialchars($item['quantity']) ?></td> <!-- Changed field name -->
                                 <td>
                                     <a href="products-edit.php?id=<?= urlencode($item['id']); ?>" class="btn btn-success btn-sm">Edit</a>
                                     <a href="products-delete.php?id=<?= urlencode($item['id']); ?>" class="btn btn-danger btn-sm">Delete</a>
@@ -92,5 +91,27 @@
         </div>
     </div>
 </div>
+<script>
+    document.querySelectorAll('.increment').forEach(button => {
+            button.addEventListener('click', function () {
+                const qtyInput = this.parentElement.querySelector('.quantityInput');
+                let quantity = parseInt(qtyInput.value);
+                if (quantity < 999) {
+                    qtyInput.value = +quantity;
+                    updateTotalPrice(this);
+                }
+            });
+        });
 
+        document.querySelectorAll('.decrement').forEach(button => {
+            button.addEventListener('click', function () {
+                const qtyInput = this.parentElement.querySelector('.quantityInput');
+                let quantity = parseInt(qtyInput.value);
+                if (quantity > 1) {
+                    qtyInput.value = -quantity;
+                    updateTotalPrice(this);
+                }
+            });
+        });
+</script>
 <?php include('includes/footer.php'); ?>
