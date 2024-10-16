668a67f4817b9897811ea3571d96531fdfd2ac64
Chingfu20 <jarinaangelo36@gmail.com>
2024-10-16 21:03:35 +0800

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -69,7 +69,7 @@
                                     <a href="products-edit.php?id=<?= urlencode($item['id']); ?>" class="btn btn-success btn-sm">Edit</a>
                                     <a href="products-delete.php?id=<?= urlencode($item['id']); ?>" class="btn btn-danger btn-sm">Delete</a>
                                     <a href="products-view.php?id=<?= urlencode($item['id']); ?>" class="btn btn-info btn-sm">View</a>
-                                    <a href="products-add.php?id=<?= urlencode($item['id']); ?>" class="btn btn-warning btn-sm">+</a>
+                                    <a href="products-edit.php?id=<?= urlencode($item['id']); ?>" class="btn btn-warning btn-sm">+</a>
                                 </td>
                             </tr>
                             <?php endforeach; ?>
