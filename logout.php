eb87579c15873ef218788e9b4bdbe8c55629d20a
Chingfu20 <jarinaangelo36@gmail.com>
2024-11-01 10:41:18 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,17 @@
 <?php 
+function setFlashMessage($type, $message) {
+    $_SESSION['sweet_alert'] = [
+        'type' => $type,
+        'message' => $message
+    ];
+}
 
 require 'config/function.php';
 
 if(isset($_SESSION['loggedIn'])){
 
     logoutSession();
+    setFlashMessage('success', 'Logout Successfully, ' . $row['name'] . '!');
     redirect('login.php','Logged Out Successfully.');
 }
 
