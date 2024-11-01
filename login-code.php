33f8daa9b5783fc16a6e479e0f35de8ac4cd76d2
Chingfu20 <jarinaangelo36@gmail.com>
2024-11-01 10:42:11 +0800

    Update login-code.php

--- login-code.php
+++ login-code.php
@@ -1,55 +1,58 @@
 <?php
 require 'config/function.php';
 
-if (isset($_POST['loginBtn']))
-{
+function setFlashMessage($type, $message) {
+    $_SESSION['sweet_alert'] = [
+        'type' => $type,
+        'message' => $message
+    ];
+}
 
+if (isset($_POST['loginBtn'])) {
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
-
-    if($email != '' && $password != '')
-     {
+    
+    if($email != '' && $password != '') {
         $query = "SELECT * FROM admins WHERE email='$email' LIMIT 1";
         $result = mysqli_query($conn, $query);
-        if ($result){
-
-            if (mysqli_num_rows($result) == 1){
-                
+        
+        if ($result) {
+            if (mysqli_num_rows($result) == 1) {
                 $row = mysqli_fetch_assoc($result);
                 $hasedPassword = $row['password'];
-
                 
-                if(!password_verify($password,$hasedPassword)){
-                    redirect('login.php','Invalid Password');
+                if(!password_verify($password,$hasedPassword)) {
+                    setFlashMessage('error', 'Invalid Password');
+                    redirect('login.php', 'Invalid Password');
                 }
-
-                if($row['is_ban'] == 1){
-                    redirect('login.php','Your account has been banned. Contact your Admin.');
+                
+                if($row['is_ban'] == 1) {
+                    setFlashMessage('error', 'Your account has been banned. Contact your Admin.');
+                    redirect('login.php', 'Your account has been banned. Contact your Admin.');
                 }
-
+                
                 $_SESSION['loggedIn'] = true;
                 $_SESSION['loggedInUser'] = [
                     'user_id' => $row['id'],
                     'name' => $row['name'],
                     'email' => $row['email'],
                     'phone' => $row['phone'],
-
                 ];
-                    
-                redirect('admin/index.php','Logged In Successfully');
-                    
-            }else{
-                    redirect('login.php', 'Invalid Email Address');
+                
+                setFlashMessage('success', 'Login Successfully, ' . $row['name'] . '!');
+                redirect('admin/index.php', 'Logged In Successfully');
+                
+            } else {
+                setFlashMessage('error', 'Invalid Email Address');
+                redirect('login.php', 'Invalid Email Address');
             }
-
-        }else{
-                redirect('login.php', 'Something Went Wrong!');
+        } else {
+            setFlashMessage('error', 'Something Went Wrong!');
+            redirect('login.php', 'Something Went Wrong!');
         }
+    } else {
+        setFlashMessage('error', 'All fields are mandatory!');
+        redirect('login.php', 'All fields are mandatory!');
     }
-    else 
-    {
-          redirect('login.php', 'All fields are mandetory!');
-    }
-    
 }
-?> 
+?>
