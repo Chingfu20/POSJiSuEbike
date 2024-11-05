ccd91ff1354076511ea935912e7f678de9769057
Chingfu20 <jarinaangelo36@gmail.com>
2024-11-05 16:30:04 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -226,7 +226,7 @@ $conn->close();
         </div>
         <div class="card-body text-center">
             <h3 id="totalOrdersText">
-                  <?php    echo htmlspecialchars($totalToday); ?>
+                  <?php    echo htmlspecialchars($orders); ?>
             </h3>
         </div>
     </div>
