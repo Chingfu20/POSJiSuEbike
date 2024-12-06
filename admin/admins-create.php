3e65aab92ec2114e49b41562bd074729c2c0e79e
Chingfu20 <jarinaangelo36@gmail.com>
2024-12-06 14:00:51 +0800

    Update admins-create.php

--- admin/admins-create.php
+++ admin/admins-create.php
@@ -28,11 +28,6 @@
                          <label for="">Phone Number *</label>
                          <input type="number" name="phone" required class="form-control" />
                     </div>
-                    <div class="col-md-3 mb-3">
-                         <label for="">Is Ban</label>
-                         <br/>
-                         <input type="checkbox" name="is_ban" style="width:30px;height:30px;" />
-                    </div>
                     <div class="col-md-12 mb-3 text-end">
                          <button type="submit" name="saveAdmin" class="btn btn-primary">Save</button>
                     </div>
