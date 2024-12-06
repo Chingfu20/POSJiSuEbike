d7caee6248191844af5880887b432814f670a91a
Chingfu20 <jarinaangelo36@gmail.com>
2024-12-06 17:31:18 +0800

    Update sidebar.php

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -8,17 +8,17 @@
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">JISU</div>
 
-                <a class="nav-link <?= $page == 'index.php' ? 'active' : ''; ?>" href="index.php">
+                <a class="nav-link <?= $page == 'index.php' ? 'active' : ''; ?>" href="index">
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
                     Dashboard
                 </a>
 
-                <a class="nav-link <?= $page == 'order-create.php' ? 'active' : ''; ?>" href="order-create.php">
+                <a class="nav-link <?= $page == 'order-create.php' ? 'active' : ''; ?>" href="order-create">
                     <div class="sb-nav-link-icon"><i class="fas fa-clipboard-list"></i></div>
                     Create Order
                 </a>
 
-                <a class="nav-link <?= $page == 'orders.php' ? 'active' : ''; ?>" href="orders.php">
+                <a class="nav-link <?= $page == 'orders.php' ? 'active' : ''; ?>" href="orders">
                     <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
                     Orders
                 </a>
@@ -36,8 +36,8 @@
                 <div class="collapse <?= ($page == 'categories-create.php' || $page == 'categories.php') ? 'show' : ''; ?>" 
                      id="collapseCategory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link <?= $page == 'categories-create.php' ? 'active' : ''; ?>" href="categories-create.php">Create Category</a>
-                        <a class="nav-link <?= $page == 'categories.php' ? 'active' : ''; ?>" href="categories.php">View Categories</a>
+                        <a class="nav-link <?= $page == 'categories-create.php' ? 'active' : ''; ?>" href="categories-create">Create Category</a>
+                        <a class="nav-link <?= $page == 'categories.php' ? 'active' : ''; ?>" href="categories">View Categories</a>
                     </nav>
                 </div>
 
@@ -52,8 +52,8 @@
                 <div class="collapse <?= ($page == 'products-create.php' || $page == 'products.php') ? 'show' : ''; ?>" 
                      id="collapseProduct" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link <?= $page == 'products-create.php' ? 'active' : ''; ?>" href="products-create.php">Create Product</a>
-                        <a class="nav-link <?= $page == 'products.php' ? 'active' : ''; ?>" href="products.php">View Products</a>
+                        <a class="nav-link <?= $page == 'products-create.php' ? 'active' : ''; ?>" href="products-create">Create Product</a>
+                        <a class="nav-link <?= $page == 'products.php' ? 'active' : ''; ?>" href="products">View Products</a>
                     </nav>
                 </div>
 
@@ -69,8 +69,8 @@
                 <div class="collapse <?= ($page == 'customers-create.php' || $page == 'customers.php') ? 'show' : ''; ?>" 
                      id="collapseCustomer" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link <?= $page == 'customers-create.php' ? 'active' : ''; ?>" href="customers-create.php">Add Customer</a>
-                        <a class="nav-link <?= $page == 'customers.php' ? 'active' : ''; ?>" href="customers.php">View Customers</a>
+                        <a class="nav-link <?= $page == 'customers-create.php' ? 'active' : ''; ?>" href="customers-create">Add Customer</a>
+                        <a class="nav-link <?= $page == 'customers.php' ? 'active' : ''; ?>" href="customers">View Customers</a>
                     </nav>
                 </div>
 
@@ -84,8 +84,8 @@
                 <div class="collapse <?= ($page == 'admins-create.php' || $page == 'admins.php') ? 'show' : ''; ?>" 
                      id="collapseAdmins" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link <?= $page == 'admins-create.php' ? 'active' : ''; ?>" href="admins-create.php">Add Admin</a>
-                        <a class="nav-link <?= $page == 'admins.php' ? 'active' : ''; ?>" href="admins.php">View Admins</a>
+                        <a class="nav-link <?= $page == 'admins-create.php' ? 'active' : ''; ?>" href="admins-create">Add Admin</a>
+                        <a class="nav-link <?= $page == 'admins.php' ? 'active' : ''; ?>" href="admins">View Admins</a>
                     </nav>
                 </div>
 
